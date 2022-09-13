03be743bdf28f65c3ddebd2ae98113e3a3ed8050
Felix Pherry <123felixpherry@gmail.com>
2022-09-13 19:45:57 +0800

    Styling done

---
+++ components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>&copy;2022 Incognito All rights reserved</p>
+      <p className="icons">
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/felix-pherry-340b86192/"
+        >
+          <AiFillLinkedin />
+        </a>
+        <a target="_blank" href="https://github.com/felix123136">
+          <AiFillGithub />
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
